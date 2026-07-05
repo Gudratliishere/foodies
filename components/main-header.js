a4c820457b631795ffa0e33004e5e7d8cebda3be
Dunay Gudratli <dqudretli@std.beu.edu.az>
2026-07-05 13:33:48 +0400

    442: style using CSS modules

---
+++ components/main-header.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import logoImg from '@/assets/logo.png'
+import styles from './main-header.module.css'
+
+export default function MainHeader() {
+    return <header className={styles.header}>
+        <Link href="/" className={styles.logo}>
+            <img src={logoImg.src} alt="NextLevel Food"/>
+            NextLevel Food
+        </Link>
+        <nav className={styles.nav}>
+            <ul>
+                <li>
+                    <Link href="/meals">Browse Meals</Link>
+                </li>
+                <li>
+                    <Link href="/commmunity">Foodies Community</Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+}
