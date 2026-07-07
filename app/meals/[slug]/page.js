1c01f9a6746834a669e17ed23b06bf8225345d4e
Dunay Gudratli <dqudretli@std.beu.edu.az>
2026-07-07 17:35:36 +0400

    474: add dynamic metadata

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,41 +1,49 @@
-import  styles from './page.module.css'
+import styles from './page.module.css'
 import Image from "next/image";
-import {getMeal, getMeals} from "@/lib/meals";
+import {getMeal} from "@/lib/meals";
 import {notFound} from "next/navigation";
 
-export const metadata = {
-  title: 'All Meals',
-  description: 'Browse a delicious meals shared by our vibrant community.',
-};
+export async function generateMetadata({params}) {
+    const meal = getMeal(params.slug);
 
-export default function MealPage({ params }) {
-  const meal = getMeal(params.slug);
+    if (!meal) {
+        notFound();
+    }
 
-  if (!meal) {
-    return notFound();
-  }
+    return {
+        title: `Meal: ${meal.title}`,
+        description: meal.summary
+    }
+}
 
-  meal.instructions = meal.instructions.replace(/\n/g, '<br/>');
+export default function MealPage({params}) {
+    const meal = getMeal(params.slug);
 
-  return <>
-    <header className={styles.header}>
-      <div className={styles.image}>
-        <Image src={meal.image} alt={meal.title} fill/>
-      </div>
-      <div className={styles.headerText}>
-        <h1>{meal.title}</h1>
-        <p className={styles.creator}>
-          by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
-        </p>
-        <p className={styles.summary}>
-          {meal.summary}
-        </p>
-      </div>
-    </header>
-    <main>
-      <p className={styles.instructions} dangerouslySetInnerHTML={{
-        __html: meal.instructions
-      }}></p>
-    </main>
-  </>
+    if (!meal) {
+        return notFound();
+    }
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br/>');
+
+    return <>
+        <header className={styles.header}>
+            <div className={styles.image}>
+                <Image src={meal.image} alt={meal.title} fill/>
+            </div>
+            <div className={styles.headerText}>
+                <h1>{meal.title}</h1>
+                <p className={styles.creator}>
+                    by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                </p>
+                <p className={styles.summary}>
+                    {meal.summary}
+                </p>
+            </div>
+        </header>
+        <main>
+            <p className={styles.instructions} dangerouslySetInnerHTML={{
+                __html: meal.instructions
+            }}></p>
+        </main>
+    </>
 }
