1e2e9ec9adacd90beba7b33f32d85d9b2481ca04
Dunay Gudratli <dqudretli@std.beu.edu.az>
2026-07-07 11:50:34 +0400

    463: store server actions in separate files

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,22 +1,8 @@
 import classes from './page.module.css';
 import ImagePicker from "@/components/meals/image-picker";
+import {shareMeal} from "@/lib/actions";
 
 export default function ShareMealPage() {
-  async function shareMeal(formData) {
-    'use server';
-
-    const meal = {
-      title: formData.get('title'),
-      creator: formData.get('name'),
-      creator_email: formData.get('email'),
-      summary: formData.get('summary'),
-      instructions: formData.get('instructions'),
-      image: formData.get('image'),
-    }
-
-    console.log(meal);
-  }
-
   return (
       <>
         <header className={classes.header}>
